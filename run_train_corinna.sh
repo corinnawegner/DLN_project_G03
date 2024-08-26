f9b6b74fea8c4e0a25a38fcc09883b819799ee6f
Corinna Wegner <corinna.wegner@outlook.de>
2024-08-26 23:24:42 +0200

    default nothing

--- run_train_corinna.sh
+++ run_train_corinna.sh
@@ -38,5 +38,5 @@ echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 # Run the script:
 #python -u bart_generation.py --use_gpu --local_files_only --option finetune --task sst --hidden_dropout_prob 0.1
 #srun python -u multitask_classifier_task.py --use_gpu --local_files_only --option finetune --additional_input --task multitask
-srun python -u bart_generation.py --use_gpu --normal_mode
+srun python -u bart_generation.py --use_gpu
 #srun python -u bart_detection.py
